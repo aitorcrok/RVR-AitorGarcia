05ec724ddff1eda5a93986654cf24ef7b72a57ac
aitorcrok <aitorcrok>
2021-05-14 11:43:19 +0200

    ej 3 arreglo + ej 5 done

--- practica2.1/ejercicio5.cc
+++ practica2.1/ejercicio5.cc
@@ -5,36 +5,33 @@
 #include <string.h>
 
 #include <iostream>
+#include <stdio.h>
 /*
- *  ./time_client <dir.> <puerto> <comando>
+ *  ./echo_client <dir. escucha> <puerto>
  */
 int main(int argc, char** argv)
 {
-    if(argc != 4)
+    if(argc != 3)
     {
-        std::cerr << "Error: Deben introducirse tres parámetros, dirección, puerto y comando\n";
+        std::cerr << "Error: Deben introducirse dos parámetros, dirección y puerto\n";
         return -1;
     }
-
-    struct sockaddr* servaddr;
     struct addrinfo hints;
     struct addrinfo* res;
 
-    memset(&servaddr, 0, sizeof(struct sockaddr));
     memset((void*) &hints, 0, sizeof(struct addrinfo));
 
     hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_socktype = SOCK_STREAM;
 
     int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+
     if(rc != 0)
     {
         std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << std::endl;
         return -1;
     }
 
-    servaddr = res->ai_addr;
-       
     int sd = socket(res->ai_family, res->ai_socktype, 0);
 
     if(sd == -1)
@@ -43,22 +40,30 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    freeaddrinfo(res);
 
-    char buffer[12];
-    socklen_t servlen = sizeof(struct sockaddr);
-
-    char* comm = argv[3];
-    
-    sendto(sd, comm, sizeof(comm), 0, servaddr, servlen);
-
-    if(comm[0] == 't' || comm[0] == 'd')
+    int con = connect(sd, res->ai_addr, (socklen_t) res->ai_addrlen);
+    if(con == -1)
     {
-        int bytes = recvfrom(sd, (void *) &buffer, 12, 0, servaddr, &servlen);
+        std::cerr << "[connect]: conexion fallida" << std::endl;
+    }
+
+    freeaddrinfo(res);
+    
+    while(true){
+        char buffer[80];
+
+        fgets(buffer, 80, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        send(sd, buffer, 79, 0);
+
+        if(buffer[0] == 'Q' && buffer[1] == '\0'){
+            break;
+        }
+
+        int bytes = recv(sd, (void *) buffer, 79, 0);
         buffer[bytes] = '\0';
         std::cout << buffer << std::endl;
     }
-
     close(sd);
 
     return 0;
